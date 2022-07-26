4f473c1b962fe8b5fac4e49598c7d12b80235547
LuckyK <88355964+aL0ver@users.noreply.github.com>
2022-07-26 12:18:54 -0500

    Create Punto 1.py

---
+++ Punto 1.py
@@ -0,0 +1,11 @@
+name = input('Ingrese su nombre: ')
+lastname = input('Ingrese su apellido: ')
+phoneNum = input('Ingrese su numero telefonico: ')
+age = input('Ingrese una fecha importante: ')
+contacto = {
+    'nombre' : name,
+    'apellido' : lastname,
+    'telefono' : phoneNum,
+    'fecha_importante' : age
+}
+print(contacto)
